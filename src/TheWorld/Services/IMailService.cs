5d33b0cd7f43bd0b7d1c3c238c0030a3f830fd00
Steve Torchia <storchia@ioninteractive.com>
2016-04-04 13:30:02 -0400

    initial

---
+++ src/TheWorld/Services/IMailService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Services
+{
+    public interface IMailService
+    {
+        bool SendMail(string to, string from, string subject, string body);
+
+    }
+}
---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,56 @@
+// site.js
+(function startup() {
+
+    ////var ele = document.getElementById("username");
+    ////ele.innerHTML = "Steve T";
+    //var ele = $("#username");
+    //ele.text("Steve T");
+
+    //// var main = document.getElementById("main");
+    //var main = $("#main");
+    ////main.onmouseenter = function() {
+    ////    main.style = "background-color: #888; ";
+    ////}
+    ////main.on("mouseenter", function() {
+    ////    main[0].style = "background-color: #888; ";
+    ////});
+
+    //main.on("mouseenter", mouseEnter);
+    //main.on("mouseleave", mouseLeave);
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", menuClick);
+
+    function mouseLeave() {
+        this.style = "";
+    }
+
+    function mouseEnter() {
+        this.style = "background-color: #888; ";
+    }
+
+    //function menuClick() {
+    //    var me = this;
+    //    alert(me.text);
+    //}
+
+    var $icon = $("#sidebarToggle i.fa");
+
+    $("#sidebarToggle").on("click", function() {
+        var $sidebarAndWrapper = $("#sidebar, #wrapper");
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        } else {
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
+        }
+    });
+
+    $("#main").on("mouseenter", mouseEnter);
+    $("#main").on("mouseleave", mouseLeave);
+
+})();
+
+ 
